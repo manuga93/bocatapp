91ddba4eef943a74a0863afddd797dbfb95dbd6f
Andres Lopez <andres.albin93@gmail.com>
2017-03-07 22:33:10 +0100

    inicializado proyecto con todos los modulos

--- bocatapp/settings.py
+++ bocatapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bocatapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,3 +103,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
--- bocatapp/urls.py
+++ bocatapp/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     # url(r'^$', 'bocatapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^admin/', admin.site.urls),
+    url(r'^/seller', include('seller.urls')),
+    url(r'^administration/', include('administrator.urls'),
+    url(r'^customer/', include('customer.urls'))
+
 ]
