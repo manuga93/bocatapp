31bf8a8f0dc8925be73fba5f96188258128c767e
Andres Garcia <andgarq@gmail.com>
2017-03-13 19:55:55 +0100

    ADD: preview menu

--- seller/urls.py
+++ seller/urls.py
@@ -1,4 +1,9 @@
+from django.conf.urls import include, url
+
 urlpatterns = [
     # Users URLs
     #url(r'^register$', RegistrationView.as_view(), name='guest_register')
+    # url(r'^$', 'bocatapp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^carta/$', 'seller.views.carta')
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from seller.models import Product
+from django.shortcuts import get_list_or_404, render_to_response
 
 # Create your views here.
+
+
+
+def carta(request): #Recibira una id de local def carta(request, local_id)
+    productos = Product.objects.all() #productos = get_list_or_404(Producto, fk=local_id)
+    return render_to_response('carta.html', 
+                                {'productos': productos})
