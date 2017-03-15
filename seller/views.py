d33d23f2584cfebf7ef44080ad6b01f13da859c9
Hugo Ramos <hugoramosrico@gmail.com>
2017-03-15 01:35:29 +0100

    CRUD de Locales

--- seller/views.py
+++ seller/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from seller.models import Product
-from django.shortcuts import get_list_or_404, render_to_response
+from seller.models import Product, Local
+from django.shortcuts import get_list_or_404, render_to_response, render, redirect, get_object_or_404
+from forms.forms import LocalForm
 
 # Create your views here.
 
@@ -10,3 +11,46 @@ def carta(request): #Recibira una id de local def carta(request, local_id)
     productos = Product.objects.all() #productos = get_list_or_404(Producto, fk=local_id)
     return render_to_response('carta.html', 
                                 {'productos': productos})
+
+
+# Vista para el lisstado de locales
+def local_list(request):
+        locals = Local.objects.all()
+        return render(request, 'local_list.html', {'locals':locals})
+
+# Vista para la creacion de un nuevo local
+def local_new(request):
+    
+    if request.method == "POST":
+        form = LocalForm(request.POST)
+        if form.is_valid():
+            local = form.save(commit=False)
+            local.seller = request.user
+            local.isActive = True
+            local.save()
+            return redirect('seller.views.local_detail', pk=local.pk)
+    else:
+        form = LocalForm()
+    
+    return render(request, 'local_edit.html', {'form': form})
+
+# Vista para los detalles de un local
+def local_detail(request, pk):
+    local = get_object_or_404(Local, pk=pk)
+    return render(request, 'local_detail.html', {'local': local})
+
+# Vista para la creacedicion de un local
+def local_edit(request, pk):
+    local = get_object_or_404(Local, pk=pk)
+    if request.method == "POST":
+        form = LocalForm(request.POST, instance=local)
+        if form.is_valid():
+            local = form.save(commit=False)
+            local.seller = request.user
+            local.isActive = True
+            local.save()
+            return redirect('seller.views.local_detail', pk=local.pk)
+    else:
+        form = LocalForm(instance=local)
+    
+    return render(request, 'local_edit.html', {'form': form})
