a56ca6fc25656b8d981aec60051500aaae6a2da2
dylanforit <dylanmorenotellez@gmail.com>
2017-03-19 19:20:10 +0100

    cambio en patornes de los numeros de la tarjetas

--- administration/services/CreditCardService.py
+++ administration/services/CreditCardService.py
@@ -8,19 +8,22 @@ def get_cc_type(number):
     number = str(number)
     #group checking by ascending length of number
     if len(number) == 13:
-      if number[0] == "4":
-        return "Visa"
+        if number[0] == "4":
+            return "Visa"
     elif len(number) == 14:
-      if number[:2] == "36":
-        return "MasterCard"
+        if number[:2] == "36":
+            return "MasterCard"
     elif len(number) == 15:
-      if number[:2] in ("34", "37"):
-        return "American Express"
+        if number[:2] in ("34", "37"):
+            return "American Express"
     elif len(number) == 16:
-      if number[:4] == "6011":
-        return "Discover"
-      if number[:2] in ("51", "52", "53", "54", "55"):
-        return "MasterCard"
-      if number[0] == "4":
-        return "Visa"
+        if number[:4] == "6011":
+            return "Discover"
+        if number[:2] in ("51", "52", "53", "54", "55"):
+            return "MasterCard"
+        if number[0] == "4":
+            return "Visa"
+    elif len(number) == 19:
+        if number[0] == "4":
+            return "Visa"
     return "Unknown"
--- administration/views.py
+++ administration/views.py
@@ -3,12 +3,10 @@ from forms.forms import CreditCardForm
 from services.CreditCardService import get_cc_type
 from django.shortcuts import get_list_or_404, render_to_response, render, redirect, get_object_or_404
 
+
+
 def creditcard_new(request):
     form = CreditCardForm(request.POST)
-
-
-
-
     if request.method == "POST":
         form = CreditCardForm(request.POST)
         if form.is_valid():
@@ -22,7 +20,3 @@ def creditcard_new(request):
 
     return render(request, 'creditcard_edit.html', {'form': form})
 
-# Vista para el lisstado de creditCards
-def creditCard_list(request):
-        creditCards = CreditCard.objects.all()
-        return render(request, 'creditCard_list.html', {'creditCards':creditCards})
