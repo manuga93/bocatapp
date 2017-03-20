33f6d0277cb8caa3cda0de23efc859558eb4672c
Andres Garcia <andgarq@gmail.com>
2017-03-20 18:55:26 +0100

    Unificacion de populate, cambios de modelo modulo customer

--- bocatapp/management/commands/populatedb.py
+++ bocatapp/management/commands/populatedb.py
@@ -4,6 +4,7 @@ from django.core.management.base import BaseCommand
 from bocatapp.models import User
 from django.contrib.auth.models import Permission
 from seller.models import Local, Product
+from customer.models import Order, CreditCard, OrderLine
 
 
 # Los archivos que se encuentren en el paquete commands, se podr�n llamar
@@ -99,6 +100,55 @@ class Command(BaseCommand):
         product2_local2.save()
 
         print ('Products...Ok!')
+
+         # ==============================================================================================================
+        
+        creditCard = CreditCard(
+            holderName='Paco Perez',
+            brandName='visa',
+            expireMonth = '12',
+            expireYear = '2020',
+            cvv = '123',
+            number = '4528348244106025',
+            user=customer1)
+        creditCard.save()
+
+        print('creditCard... Ok!')
+        
+         # ==============================================================================================================
+
+        order1 = Order(totalPrice=2.10, moment='2017-04-01 14:35:00',local=local1,
+                        comment="Sin salsas",customer=customer1,creditCard=creditCard,
+                        pickupMoment='2017-04-01 14:45:00')
+        order1.save()
+
+        order2 = Order(totalPrice=5.10, moment='2017-04-01 14:30:00', local=local1,
+                       comment="Mucho roquefort", customer=customer1, creditCard=creditCard,
+                       pickupMoment='2017-04-01 15:00:00')
+        order2.save()
+
+        order3 = Order(totalPrice=6.10, moment='2017-04-01 14:40:00', local=local2,
+                       comment="Lo quiero todo rapido", customer=customer2, creditCard=creditCard,
+                       pickupMoment='2017-04-01 14:55:00')
+        order3.save()
+        print("Orders... Ok!")
+
+         # ==============================================================================================================
+
+        order_line1 = OrderLine(quantity=1, name="Bocadillo de Pavo", price=2.10, order=order1)
+        order_line1.save()
+
+        order_line2 = OrderLine(quantity=1, name="Lomo con Roquefort", price=3.10, order=order2)
+        order_line2.save()
+
+        order_line3 = OrderLine(quantity=1, name="Donut chocolate", price=2.00, order=order2)
+        order_line3.save()
+
+        order_line4 = OrderLine(quantity=2, name="Bocadillo hipergigante", price=3.05, order=order3)
+        order_line4.save()
+
+        print("OrdersLine... Ok!")
+
 
         print ('Populated...Ok!')
 
