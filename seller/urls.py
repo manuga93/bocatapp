e6a39a8f0588145038498562d5c8d822c9cb7405
Andres Garcia <andgarq@gmail.com>
2017-03-20 16:09:59 +0100

    Registro producto y local

--- seller/urls.py
+++ seller/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     # url(r'^$', 'bocatapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^carta/$', 'seller.views.carta'),
+    url(r'^carta/$', 'seller.views.menu_list'),
     url(r'^local$', 'seller.views.local_list'),
+    url(r'^product/new/$', 'seller.views.product_new', name='product_new'),
+    url(r'^category/new/$', 'seller.views.category_new', name='category_new'),
     url(r'^local/new/$', 'seller.views.local_new', name='local_new'),
     url(r'^local/(?P<pk>[0-9]+)/$', 'seller.views.local_detail'),
     url(r'^local/(?P<pk>[0-9]+)/edit/$', 'seller.views.local_edit', name='local_edit'),
--- seller/views.py
+++ seller/views.py
@@ -1,20 +1,54 @@
 from django.http import HttpResponse
 
-from seller.models import Product, Local
+from seller.models import Product, Local, Category
 from django.shortcuts import get_list_or_404, render_to_response, render, redirect, get_object_or_404
-from forms.forms import LocalForm
+from forms.forms import LocalForm, CategoryForm, ProductForm
 
 
 # Create your views here.
 
 
-
-def carta(request): #Recibira una id de local def carta(request, local_id)
-    productos = Product.objects.all() #productos = get_list_or_404(Producto, fk=local_id)
+# Lista el menu de productos de un local
+def menu_list(request, local_id): 
+    productos = get_list_or_404(Product, local_pk = local_id) 
     return render_to_response('carta.html', 
                                 {'productos': productos})
 
 
+# Vista para la creacion de una nueva categoria
+
+def category_new(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.save()
+            return redirect('seller.views.local_new')
+    else:
+        form = CategoryForm()
+        
+    return render(request, 'category_edit.html', {'form': form})
+
+# Vista para la creacion de un nuevo producto (a falta de seleccionar el local)
+
+def product_new(request):
+    
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.save()
+            return redirect('seller.views.local_new')
+    else:
+        form = ProductForm()
+
+    return render(request, 'product_edit.html', {'form': form})
+
+# Vista para el listado de categorias y productos
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'category_list.html', {'categories': categories})
 
 # Vista para el lisstado de locales
 def local_list(request):
