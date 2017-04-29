c7b5949e5139783fb27107292b865ee4b6919890
Andres Lopez Albin <andres.albin93@gmail.com>
2017-04-29 15:35:42 +0200

    Creacion de controlador para editar contraseña

--- bocatapp/views/UserAccount.py
+++ bocatapp/views/UserAccount.py
@@ -42,3 +42,35 @@ class UserEdit(edit.BaseUpdateView):
                 return render(request, '../templates/forms/user_edit.html', context)
         else:
             return render(request, '../templates/forbidden.html')
+
+
+class PasswordEdit(edit.BaseUpdateView):
+    def get(self, request):
+        if request.user.is_authenticated():
+            user_form = UserForm(instance=request.user, prefix='user')
+            context = {
+                'type': 'Editar perfil',
+                'user_form': user_form
+            }
+            return render(request, '../templates/forms/user_edit.html', context)
+
+    @transaction.atomic
+    def post(self, request):
+        if request.user.is_authenticated():
+            user_form = UserForm(data=request.POST, instance=request.user, prefix='user')
+            if user_form.is_valid():
+                user = user_form.save(commit=False)
+                user.save()
+                return render(request, '../templates/myaccount.html')
+            else:
+                message = ""
+                for field, errors in (user_form.errors.items()):
+                    for error in errors:
+                        message += error
+                context = {
+                    'user_form': user_form,
+                    'message': message
+                }
+                return render(request, '../templates/forms/user_edit.html', context)
+        else:
+            return render(request, '../templates/forbidden.html')
