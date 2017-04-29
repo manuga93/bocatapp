58607ebaeb5f0c5762cb2a1061d928ec8ca7833d
Andres Lopez Albin <andres.albin93@gmail.com>
2017-04-29 14:43:57 +0200

    Correccion para poder editar usuario

--- bocatapp/models.py
+++ bocatapp/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-default = "http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
-
-
 class User(AbstractUser):
     amount_money = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     phone = models.CharField(max_length=14)
     birth_date = models.DateField(null=True, blank=True)
-    avatar = models.URLField(default=default)
+    avatar = models.URLField(blank=True)
 
     class Meta:
         db_table = 'auth_user'
