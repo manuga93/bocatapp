39a1c4c8ae0dfd7470279cd7681ab6f83fd1ce61
Manuel Garrido <manuga93@gmail.com>
2017-07-07 22:31:40 +0200

    FIX: CVV credit card validator

--- administration/models.py
+++ administration/models.py
@@ -11,7 +11,7 @@ class CreditCard(models.Model):
     brandName = models.CharField(max_length=32)
     expireMonth = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)])
     expireYear = models.IntegerField(validators=[MinValueValidator(2017)])
-    cvv = models.IntegerField(validators=[MinValueValidator(12), MaxValueValidator(999)])
+    cvv = models.IntegerField(validators=[MinValueValidator(10), MaxValueValidator(9999)])
     number = models.CharField(max_length=32,validators=[CreditCardService.luhn])
     user = models.ForeignKey(User)
     isDeleted = models.BooleanField(default=False)
