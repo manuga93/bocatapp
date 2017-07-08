bd30ff6b0fc0674454b973511d2268f48a4b961d
Manuel Garrido <manuga93@gmail.com>
2017-07-08 13:09:54 +0200

    FIX: cvv code

--- administration/models.py
+++ administration/models.py
@@ -5,7 +5,7 @@ from administration.services import CreditCardService
 from django.utils.translation import ugettext_lazy as _
 
 alphanumeric = RegexValidator(r'^[\sa-zA-Z]*$', _('Holder name field must be contains letters'))
-
+cvvValidator = RegexValidator(r'^[0-9]{3,4}$', _('CVV must have 3 or 4 digits'))
 # Create your models here.
 
 class CreditCard(models.Model):
@@ -13,7 +13,7 @@ class CreditCard(models.Model):
     brandName = models.CharField(max_length=32)
     expireMonth = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)])
     expireYear = models.IntegerField(validators=[MinValueValidator(2017)])
-    cvv = models.IntegerField(validators=[MinValueValidator(10), MaxValueValidator(9999)])
+    cvv = models.CharField(validators=[cvvValidator], max_length=4)
     number = models.CharField(max_length=32,validators=[CreditCardService.luhn])
     user = models.ForeignKey(User)
     isDeleted = models.BooleanField(default=False)
